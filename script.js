378e3e51d33bcbfdac1d94bd6adac0f1484a89c7
Lucas Ferreira <lucasdsferreira11@gmail.com>
2024-12-13 17:39:16 -0300

    att

---
+++ script.js
@@ -0,0 +1,16 @@
+    let lastScroll = 0;
+    const header = document.querySelector("header");
+
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+
+        if (currentScroll > lastScroll && currentScroll > 50) {
+            // Rolando para baixo e fora do topo
+            header.style.top = "-100px"; // Esconde o header
+        } else {
+            // Rolando para cima ou parando
+            header.style.top = "0"; // Mostra o header
+        }
+
+        lastScroll = currentScroll;
+    });
